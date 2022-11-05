d2f19218f2abea50aa7164b9cd74023aa40cd728
Samuel Costa Ferreira <106633009+samuhcosta@users.noreply.github.com>
2022-11-04 21:24:29 -0300

    Formula com função

--- imc.py
+++ imc.py
@@ -1,13 +1,16 @@
-print("_____Programa para calcular seu IMC_____")
-peso = eval(input("Digite seu peso em (ex= kg): "))
-altura = eval(input("Digite sua altura (ex=1.75): "))
-imc = peso/altura**2
+def imc(peso, altura):
+    imc = peso/altura**2
+    if imc < 18.5:
+        print("Você está abaixo do peso ideal!")
+    elif imc < 25:
+        print("Seu peso está NORMAL!")
+    elif imc >= 25:
+        print("Você está acima do peso ideal!")
+    else:
+        print("Erro, verifique o número digitado!")
 
-if imc < 18.5:
-    print("Você está abaixo do peso ideal!")
-elif imc < 25:
-    print("Seu peso está NORMAL!")
-elif imc >= 25:
-    print("Você está acima do peso ideal!")
-else:
-    print("Erro, verifique o número digitado!")
+print("Programa para calcular seu peso ideial:")
+
+peso = eval(input("Digite seu peso (ex'kg'): "))
+altura = eval(input("Digite sua altura (ex'1.74'): "))
+imc(peso, altura)
