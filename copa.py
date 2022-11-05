cd87c268359daa1ea543213352bd5081c6e19871
Samuel Costa Ferreira <106633009+samuhcosta@users.noreply.github.com>
2022-11-04 21:51:12 -0300

    criando o algoritmo da copa

---
+++ copa.py
@@ -0,0 +1,8 @@
+def copa():
+    for i in range(1966,2023,4):
+        if i == 2022:
+            print(f"No ano de {i} vai ter copa do mundo!")
+        else:
+            print(f"No ano de {i} teve Copa do mundo!")
+
+copa()
